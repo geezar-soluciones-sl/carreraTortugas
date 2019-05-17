de89d7a490ebdd2d30377b89b23041e4a4eb1007
Angel Martinez <aml@geezar.es>
2019-05-17 11:57:53 +0200

    Creación circuito y corredores

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import turtle # Que tiene también su propio lienzo. Lo usaremos (tipo Screen)
 
 class Circuito():
     corredores = []
+    posStartY = (-30,-10,10,30)
+    coloresT = ("red", "blue", "green", "orange")
 
     def __init__(self, ancho, alto):
         self.__screen = turtle.Screen()
@@ -13,16 +15,25 @@ class Circuito():
 # En Python siempre se puede con miCirc._Circuito__screen
         self.__screen.setup(ancho,alto)
         self.__screen.bgcolor("lightgray")
+        self.__lineaSalida = 20-ancho/2
+        self.__lineaLlegada = ancho/2 -20
         
+        self.__crearCorredores()
+    
+    def __crearCorredores(self):
         for i in range(4):
             newTurtle = turtle.Turtle()
+            newTurtle.shape("turtle")
+            newTurtle.penup() #"Levanta el boli" para que no se vean las lineas
+            newTurtle.setpos(self.__lineaSalida, self.posStartY[i])
+            newTurtle.color(self.coloresT[i])
+            
             self.corredores.append(newTurtle)
-
 
 # Ejecutamos algo en __main__ para ver cómo va
 
 if __name__ == "__main__":
-    circuito=Circuito(680,480)
+    circuito=Circuito(640,480)
     
 
         
